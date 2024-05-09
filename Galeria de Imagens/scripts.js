f4ccae95d282b398b534ae0f4c1f7fcace90f58f
Gabriel-Maran <gabriel.enzo.libero.maran@gmail.com>
2024-05-09 07:31:52 -0300

    Decimo commit

---
+++ Galeria de Imagens/scripts.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+alert("abc")
+p
+})
+//object: estrutura de dados que pode armazenar multiplos valores
+//NULL: quando esta vazio
+//arry: objeto especial ordenado
+//function
