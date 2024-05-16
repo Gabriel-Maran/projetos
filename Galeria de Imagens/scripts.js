22e2d7645a99fc4f162e3db9110b56e4ab9ee30b
Gabriel-Maran <gabriel.enzo.libero.maran@gmail.com>
2024-05-16 07:40:39 -0300

    algumas alterações no js novamente

--- Galeria de Imagens/scripts.js
+++ Galeria de Imagens/scripts.js
@@ -1,29 +1,37 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const notamedia = Number(prompt("Digite a media"));
-    if(notamedia == 0){
-        console.log("Aluno 100% reprovado");
-    }
-    else if(notamedia > 0 && notamedia < 6){
-        console.log("Reprovado");
-    }
-    else if(notamedia>=6 && notamedia<8){
-        console.log("Passou");
-    }
-
-    else if(notamedia>=8 && notamedia<9.9){
-        console.log("Passou com nota boa em, muito nerd");
-    }
-    else if(notamedia == 10){
-        console.log("Gênio");
-    }
-    else{
-        console.log("Digita certo, animal");
-    }
-    
+    const numero = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+    numero.forEach(num  =>{
+        console.log("num")
+    })
 })
+//objetos são coleções não ordenadas de pares chave-valor, onde cada valor é associado a uma chave única
+/*    
+    let pessoa = {
+    nome1: 'a', 
+    nome2: 'b',
+    nome3: 'c'
+    };
+    console.log(pessoa['nome3'])
+    */
+//array é ordenado, o objeto não
+/*
+    let primeiro = ["Aluno1","Aluno2", "Aluno3", "Aluno4"]
+    console.log(primeiro[1])
+*/
+//Funções são blocos de codigos que podem ser chamados e reutilizados
+//Dentro dos parenteses da função vai os parâmetros
+/*
+function funcao(parametro, parametro2) {    
+    return parametro +' 1.5 '+ parametro2;
+}
+const parametro = "1"
+const parametro2 = "2"
+console.log(funcao(parametro, parametro2))
+*/
 //object: estrutura de dados que pode armazenar multiplos valores
 //NULL: quando esta vazio
 //arry: objeto especial ordenado
 //function
 // === verifica se é igual e do mesmo tipo
 // !== se é estritamente diferente
+//const é permanente, let é moldavel
