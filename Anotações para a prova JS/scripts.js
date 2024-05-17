3c17e1e84ca8da82424a1812e5dda21eb714fe30
Gabriel-Maran <gabriel.enzo.libero.maran@gmail.com>
2024-05-17 08:26:44 -0300

    documento de js para a prova

---
+++ Anotações para a prova JS/scripts.js
@@ -0,0 +1,39 @@
+/*
+document.addEventListener('DOMContentLoaded', function () {
+    const numero = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+    numero.forEach(num  =>{
+        console.log(num)
+    })
+})
+*/
+//objetos são coleções não ordenadas de pares chave-valor, onde cada valor é associado a uma chave única
+/*    
+    let pessoa = {
+    nome1: 'a', 
+    nome2: 'b',
+    nome3: 'c'
+    };
+    console.log(pessoa['nome3'])
+    */
+//array é ordenado, o objeto não
+/*
+    let primeiro = ["Aluno1","Aluno2", "Aluno3", "Aluno4"]
+    console.log(primeiro[1])
+*/
+//Funções são blocos de codigos que podem ser chamados e reutilizados
+//Dentro dos parenteses da função vai os parâmetros
+/*
+function funcao(parametro, parametro2) {    
+    return parametro +' 1.5 '+ parametro2;
+}
+const parametro = "1"
+const parametro2 = "2"
+console.log(funcao(parametro, parametro2))
+*/
+//object: estrutura de dados que pode armazenar multiplos valores
+//NULL: quando esta vazio
+//arry: objeto especial ordenado
+//function
+// === verifica se é igual e do mesmo tipo
+// !== se é estritamente diferente
+//const é permanente, let é moldavel
