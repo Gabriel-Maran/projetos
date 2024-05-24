b5e17208d4410342acd7cf0fe5d039c9f9b45d9e
Gabriel-Maran <gabriel.enzo.libero.maran@gmail.com>
2024-05-24 09:25:49 -0300

    final do filtro de categorias no js

--- Galeria de Imagens/scripts.js
+++ Galeria de Imagens/scripts.js
@@ -1,16 +1,17 @@
 document.addEventListener('DOMContentLoaded' , function(){
     const baseImagens = {
-        todas: ['animal_1.jpg', 'animal_2.jpg', 'animal_3.jpeg', 'cidade_1.jpg', 'cidade_2.jpg', 'cidade_3.jpg', 'natureza_4.jpg'],
+        todas: ['animal_1.jpg', 'animal_2.jpg', 'animal_3.jpeg', 'animal_4.webp', 'cidade_1.jpg', 'cidade_2.jpg', 'cidade_3.jpg', 'natureza_4.jpg'],
         natureza: ['natureza_4.jpg'],
         cidade: ['cidade_1.jpg', 'cidade_2.jpg', 'cidade_3.jpg'],
-        animais: ['animal_1.jpg', 'animal_2.jpg', 'animal_3.jpeg']
+        animais: ['animal_1.jpg', 'animal_2.jpg', 'animal_3.jpeg', 'animal_4.webp']
     }
     //Abaixo, temos um evento de click, que executara um evento de clique
 
-    function carregaImagens(){
+    function carregaImagens(categoria){
         //selecionar elementos html, com base em um seletor
         const galeria = document.querySelector("#galeria-imagens");
-        const imagens = baseImagens['todas'];
+        galeria.innerHTML = '';
+        const imagens = baseImagens[categoria];
         imagens.forEach(img => { 
             galeria.innerHTML += '<div> <img src="Imagens/'+img+'" /> </div>';
          });
@@ -20,7 +21,8 @@ document.addEventListener('DOMContentLoaded' , function(){
     document.body.addEventListener('click', function (event) {
         //acessa a lista de classe de onde clicou e procura pela class botao-categoria
         if(event.target.classList.contains('botao-categoria')){
-            carregaImagens();
+            const categoria = event.target.dataset.categoria;
+            carregaImagens(categoria);
         }
     })
 })
