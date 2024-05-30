8aac042edf2cd98c235426df292000f925cb53db
Gabriel-Maran <gabriel.enzo.libero.maran@gmail.com>
2024-05-30 07:38:23 -0300

    inicio do filtro por ordem

--- Galeria de Imagens/scripts.js
+++ Galeria de Imagens/scripts.js
@@ -1,28 +1,46 @@
-document.addEventListener('DOMContentLoaded' , function(){
+document.addEventListener('DOMContentLoaded', function(){ 
     const baseImagens = {
-        todas: ['animal_1.jpg', 'animal_2.jpg', 'animal_3.jpeg', 'animal_4.webp', 'cidade_1.jpg', 'cidade_2.jpg', 'cidade_3.jpg', 'natureza_4.jpg'],
+        todas: ['animal_1.jpg', 'animal_2.jpg', 'animal_3.jpeg', 'cidade_1.jpg', 'cidade_2.jpg', 'cidade_3.jpg', 'natureza_4.jpg'],
         natureza: ['natureza_4.jpg'],
         cidade: ['cidade_1.jpg', 'cidade_2.jpg', 'cidade_3.jpg'],
-        animais: ['animal_1.jpg', 'animal_2.jpg', 'animal_3.jpeg', 'animal_4.webp']
+        animais: ['animal_1.jpg', 'animal_2.jpg', 'animal_3.jpeg']
     }
-    //Abaixo, temos um evento de click, que executara um evento de clique
 
     function carregaImagens(categoria){
-        //selecionar elementos html, com base em um seletor
+        // selecionar elementos html com base em um seletor
         const galeria = document.querySelector("#galeria-imagens");
         galeria.innerHTML = '';
         const imagens = baseImagens[categoria];
-        imagens.forEach(img => { 
-            galeria.innerHTML += '<div> <img src="Imagens/'+img+'" /> </div>';
-         });
 
-
+        imagens.forEach(img => {
+            console.log(img);
+            galeria.innerHTML += '<div class = "imagem-item"> <img src="imagens/'+img+'" /> </div>'
+        
+        })
     }
-    document.body.addEventListener('click', function (event) {
-        //acessa a lista de classe de onde clicou e procura pela class botao-categoria
+
+    function ordenaImagens(ordem){
+        const imagens = Array.from(document.querySelectorAll("#galeria-imagens .imagem-item"));
+        imagens.sort((a, b) =>{
+            console.log(a);
+        })
+    }
+
+    // Evento de clique
+    document.body.addEventListener('click', function (event){ 
+
+        //acessa a lista de classe de onde clicou e procura pela classe botao-categoria
         if(event.target.classList.contains('botao-categoria')){
             const categoria = event.target.dataset.categoria;
             carregaImagens(categoria);
         }
+    
+        if(event.target.classList.contains('botao-ordenar')){
+            const ordem = event.target.dataset.ordem;
+            ordenaImagens(ordem);  
+        }
+
     })
+
+
 })
